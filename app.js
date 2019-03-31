e2ec1f36a11048a16800c02c91e2f4e8557f183e
Sean Harte <>
2019-03-31 17:24:49 -0400

    - added samle of a get request

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
 'use strict';
 
+const apiKey = 'j8OeOdziRcOtBpPMuvUR75hZIvEpPDbm9X95lDQV';
 const Hapi = require('hapi');
 const fs = require('fs');
 const imagesFS = './public/images';
-let imgFileList;
 
+const https = require('https');
+
+https.get(`https://api.nasa.gov/planetary/apod?api_key=${apiKey}`, (resp) => {
+    let data = '';
+
+    // A chunk of data has been recieved.
+    resp.on('data', (chunk) => {
+        data += chunk;
+    });
+
+    // The whole response has been received. Print out the result.
+    resp.on('end', () => {
+        console.log(JSON.parse(data).explanation);
+    });
+
+}).on("error", (err) => {
+    console.log("Error: " + err.message);
+});
 
 
 //init server
